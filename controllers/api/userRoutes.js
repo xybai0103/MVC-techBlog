52dca3127e7f96dd2f08026404b21a6fdc5a701c
xybai0103 <xueyin0103@gmail.com>
2023-05-01 11:33:09 -0500

    seed the database

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+ require('../../models');const { User } =
 //sign up
 router.post('/', async (req, res) => {
   try {
