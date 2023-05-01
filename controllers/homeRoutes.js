5e6f668422249d6bf19fee6b94e4161d0ea3540e
xybai0103 <xueyin0103@gmail.com>
2023-05-01 16:04:41 -0500

    add '/'route in the homeRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogPostData = await blogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      blogPosts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
