21bf2181c8a63b3b4fde3e540eaad3c9b585fbd6
xybai0103 <xueyin0103@gmail.com>
2023-05-01 10:47:31 -0500

    add a wildcard route in the index.js in the controllers folder for testing

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,9 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+//a wildcard route for testing
+app.get('*', (req, res) => {
+    res.status(404).send('404 Error: Page not found');
+});
+
 module.exports = router;
