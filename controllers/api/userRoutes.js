62989399e4d901b54b960f919a1bd714d6b2fa1e
xybai0103 <xueyin0103@gmail.com>
2023-05-02 16:37:17 -0500

    modify login and signup handlebars and finish login.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-  //sign up
-  router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create(req.body);
+//sign up
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        //if signed up, automatically logged in
-        req.session.logged_in = true;
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      //if signed up, automatically logged in
+      req.session.logged_in = true;
 
-        res.status(200).json(userData);
-      });
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 //login
 router.post('/login', async (req, res) => {
   try {
