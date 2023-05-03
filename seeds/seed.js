b100df8035df21ce4c6a67cdade4acacb8e8f636
xybai0103 <xueyin0103@gmail.com>
2023-05-03 00:17:47 -0500

    modify seed.js to randomly assign a user_id and a blogpost_id to any record in the Comment Model

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, BlogPost } = require('../models');
+const { User, BlogPost, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const blogPostData = require('./blogPostData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -18,6 +19,18 @@ const seedDatabase = async () => {
     await BlogPost.create({
       ...blogPost,
       user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  const blogPosts = await BlogPost.findAll();
+
+  for (const comment of commentData) {
+    const user = users[Math.floor(Math.random() * users.length)];
+    const blogPost = blogPosts[Math.floor(Math.random() * blogPosts.length)];
+    await Comment.create({
+      ...comment,
+      user_id: user.id,
+      blogpost_id: blogPost.id
     });
   }
 
